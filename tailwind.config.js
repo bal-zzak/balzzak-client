b8df3cd3e8ea66824d47640149a33086ba39e01f
Jihye <kimji9663@nate.com>
2021-09-13 17:55:12 +0900

    tailwind.config.js 설정

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["NotoSans", "sans-serif"],
+      serif: ["serif"]
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
